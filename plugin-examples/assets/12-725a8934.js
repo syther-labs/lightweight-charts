3e896deb85a0d8ca73d22845ee1310549d636c6f
TradingView <noreply@tradingview.com>
2024-12-02 12:39:47 +0000

    [skip ci] Deploy website on ad7c9192acc09cf31428606a1f98e63642c24433 ad7c9192acc09cf31428606a1f98e63642c24433

--- plugin-examples/assets/12-725a8934.js
+++
@@ -1,1 +0,0 @@
-import{V as i}from"./lightweight-charts.production-30f3f9d8.js";import{a}from"./sample-data-c7d89ab8.js";import{C as r}from"./highlight-bar-crosshair-e49788ac.js";import"./positions-0a54307c.js";const e=window.chart=i("chart",{autoSize:!0}),t=e.addCandlestickSeries();t.setData(a());const o=new r({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(o);
