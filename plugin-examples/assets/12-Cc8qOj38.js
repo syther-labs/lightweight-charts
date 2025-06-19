ccd815ab366a2c88c395e2959c18f270e8609eb2
TradingView <noreply@tradingview.com>
2025-06-19 13:50:14 +0000

    [skip ci] Deploy website on d744f0b5d0dba8cd9d84510ead6f8dd34218032b d744f0b5d0dba8cd9d84510ead6f8dd34218032b

---
+++ plugin-examples/assets/12-Cc8qOj38.js
@@ -0,0 +1,1 @@
+import{N as i,x as a}from"./lightweight-charts.production-CO6aMvaB.js";import{a as r}from"./sample-data-x_8-FZm1.js";import{C as e}from"./highlight-bar-crosshair-jp5a4dmU.js";import"./positions-t2UKkghX.js";const o=window.chart=i("chart",{autoSize:!0}),t=o.addSeries(a);t.setData(r());const s=new e({color:"rgba(0, 50, 100, 0.2)"});t.attachPrimitive(s);
