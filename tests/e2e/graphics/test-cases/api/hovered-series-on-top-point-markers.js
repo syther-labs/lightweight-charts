51abd06a17bbf1b39d3c96dded541e72d9a742b4
Andrii Ovcharenko <aovcharenko@tradingview.com>
2026-04-13 14:51:28 +0200

    update test cases

--- tests/e2e/graphics/test-cases/api/hovered-series-on-top-point-markers.js
+++ tests/e2e/graphics/test-cases/api/hovered-series-on-top-point-markers.js
@@ -1,3 +1,10 @@
+/*
+	This test checks that the bottom series is drawn above the top series
+	when hovering over a 'pointMarker'. Since both lines (series) are on the
+	same point normally the top series would be drawn on top, however we
+	consider series with point markers to be a higher importance and this will
+	promote it to the top.
+*/
 let hoverPoint;
 
 function initialInteractionsToPerform() {
@@ -13,11 +20,13 @@ function runTestCase(container) {
 
 	const hoveredSeries = chart.addSeries(LightweightCharts.LineSeries, {
 		color: '#ff0000',
-		lineWidth: 3,
+		lineWidth: 8,
+		pointMarkersVisible: true,
+		pointMarkersRadius: 6,
 	});
 	const topSeries = chart.addSeries(LightweightCharts.LineSeries, {
 		color: '#0000ff',
-		lineWidth: 6,
+		lineWidth: 8,
 	});
 
 	const hoveredSeriesData = [
@@ -28,11 +37,11 @@ function runTestCase(container) {
 		{ time: '2020-01-05', value: 40 },
 	];
 	const topSeriesData = [
-		{ time: '2020-01-01', value: 80 },
+		{ time: '2020-01-01', value: 85 },
 		{ time: '2020-01-02', value: 70 },
-		{ time: '2020-01-03', value: 70 },
+		{ time: '2020-01-03', value: 60 },
 		{ time: '2020-01-04', value: 50 },
-		{ time: '2020-01-05', value: 40 },
+		{ time: '2020-01-05', value: 45 },
 	];
 
 	hoveredSeries.setData(hoveredSeriesData);
